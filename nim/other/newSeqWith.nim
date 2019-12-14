e4d6a701c21ae707b1f1f1ede08e9b30218e3a79
chaemon <chaemon@gmail.com>
2019-12-15 01:15:10 +0900

    コミット

--- nim/other/newSeqWith.nim
+++ nim/other/newSeqWith.nim
@@ -9,13 +9,3 @@ proc newSeqWithImpl[T](lens: seq[int]; init: T; currentDimension, lensLen: stati
 template newSeqWith*[T](lens: varargs[int]; init: T): untyped =
   static: assert(lens.allIt(it > 0))
   newSeqWithImpl(@lens, init, 1, lens.len)
-
-proc reduce_consective[T](v:seq[T]): seq[(T,int)] =
-  result = newSeq[(T,int)]()
-  var i = 0
-  while i < v.len:
-    var j = i
-    while j < v.len and v[i] == v[j]: j += 1
-    result.add((v[i], j - i))
-    i = j
-  discard
--- nim/template/atcoder-tools_template.nim
+++ nim/template/atcoder-tools_template.nim
@@ -19,15 +19,15 @@ proc nextString(): string =
     else:
       if get: break
       get = false
-type someSignedInt = int|int8|int16|int32|int64|BiggestInt
-type someUnsignedInt = uint|uint8|uint16|uint32|uint64
-type someInteger = someSignedInt|someUnsignedInt
-type someFloat = float|float32|float64|BiggestFloat
+type SomeSignedInt = int|int8|int16|int32|int64|BiggestInt
+type SomeUnsignedInt = uint|uint8|uint16|uint32|uint64
+type SomeInteger = SomeSignedInt|SomeUnsignedInt
+type SomeFloat = float|float32|float64|BiggestFloat
 template `max=`*(x,y:typed):void = x = max(x,y)
 template `min=`*(x,y:typed):void = x = min(x,y)
 template inf(T): untyped = 
-  when T is someFloat: T(Inf)
-  elif T is someInteger: ((T(1) shl T(sizeof(T)*8-2)) - (T(1) shl T(sizeof(T)*4-1)))
+  when T is SomeFloat: T(Inf)
+  elif T is SomeInteger: ((T(1) shl T(sizeof(T)*8-2)) - (T(1) shl T(sizeof(T)*4-1)))
   else: assert(false)
 
 proc sort[T](v: var seq[T]) = v.sort(cmp[T])
