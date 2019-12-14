76e6f4bc4e95af68569036074d59f10df7623687
chaemon <chaemon@gmail.com>
2019-12-14 22:14:22 +0900

    atcoder-tools_templateを追加

---
+++ nim/template/atcoder-tools_template.nim
@@ -0,0 +1,77 @@
+{{"#{{{ header"}}
+{.hints:off checks:off.}
+import algorithm, sequtils, tables, macros, math, sets, strutils
+when defined(MYDEBUG):
+  import header
+
+proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
+proc getchar(): char {.header: "<stdio.h>", varargs.}
+proc nextInt(): int = scanf("%lld",addr result)
+proc nextFloat(): float = scanf("%lf",addr result)
+proc nextString(): string =
+  var get = false
+  result = ""
+  while true:
+    var c = getchar()
+    if int(c) > int(' '):
+      get = true
+      result.add(c)
+    else:
+      if get: break
+      get = false
+type someSignedInt = int|int8|int16|int32|int64|BiggestInt
+type someUnsignedInt = uint|uint8|uint16|uint32|uint64
+type someInteger = someSignedInt|someUnsignedInt
+type someFloat = float|float32|float64|BiggestFloat
+template `max=`*(x,y:typed):void = x = max(x,y)
+template `min=`*(x,y:typed):void = x = min(x,y)
+template inf(T): untyped = 
+  when T is someFloat: T(Inf)
+  elif T is someInteger: ((T(1) shl T(sizeof(T)*8-2)) - (T(1) shl T(sizeof(T)*4-1)))
+  else: assert(false)
+
+proc sort[T](v: var seq[T]) = v.sort(cmp[T])
+
+proc discardableId[T](x: T): T {.discardable.} =
+  return x
+macro `:=`(x, y: untyped): untyped =
+  if (x.kind == nnkIdent):
+    return quote do:
+      when declaredInScope(`x`):
+        `x` = `y`
+      else:
+        var `x` = `y`
+      discardableId(`x`)
+  else:
+    return quote do:
+      `x` = `y`
+      discardableId(`x`)
+{{"#}}}"}}
+
+{% if mod %}
+let MOD = {{ mod }}
+{% endif %}
+{% if yes_str %}
+let YES = "{{ yes_str }}"
+{% endif %}
+{% if no_str %}
+let NO = "{{ no_str }}"
+{% endif %}
+
+{% if prediction_success %}
+proc solve({{ formal_arguments }}) =
+  return
+{% endif %}
+
+{{"#{{{ main function"}}
+proc main() =
+{% if prediction_success %}
+  {{input_part}}
+  solve({{ actual_arguments }});
+{% else %}
+# Failed to predict input format
+{% endif %}
+  return
+
+main()
+{{"#}}}"}}
